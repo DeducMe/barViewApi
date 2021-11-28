541e01ca484e89d4ffffc096e54057c3f4dd6897
DeducMe <deducme@gmail.com>
2021-11-28 05:46:38 +0300

    unknown

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ function putMenu(sendBack, data) {
     `
   UPDATE organizationMenu
   SET id=s.id, category=s.category, title=s.title, image=s.image, description=s.description, price=s.price
-  from unnest(array[%L]) s (id BIGINT, category VARCHAR(255), title VARCHAR(255), image VARCHAR(255), description VARCHAR(255), price VARCHAR(255))
+  from unnest(array[%L]) s (id unknown, category VARCHAR(255), title VARCHAR(255), image VARCHAR(255), description VARCHAR(255), price VARCHAR(255))
   WHERE organizationMenu.id = s.id`,
     data
   );
